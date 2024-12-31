c5578a99faed007ecae122e6ef3ab106768f6c57
Mauricio Rojas Externo <mauricio.rojas.externo@zeleri.com>
2024-12-30 23:13:24 -0300

    chore: adding basic encript process

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use encryptor::Encryptable;
+use std::io;
+
+pub mod encryptor;
+
+fn main() {
+    println!("Input the string you want to encrypt:");
+
+    let mut user_input = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Cannot read input");
+    println!(
+        "Your encrypted string: {}",
+        encryptor::rot13::Rot13(user_input).encrypt()
+    );
+}
